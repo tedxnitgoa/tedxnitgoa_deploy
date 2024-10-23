7fdaacac5d3566241c296c84bd0fdebf2b250dc9
perliaryan <perliaryannageswararao@gmail.com>
2024-10-23 19:33:37 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:process.env.FRONTEND_URL || 'http://localhost:3000'
+  origin:process.env.FRONTEND_URL || 'http://localhost:3000',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 }));
 app.use(express.json());
 app.use(helmet());
@@ -50,6 +51,9 @@ const validateOrderInput = [
 ];
 
 // Routes
+app.get("/health",()=>{
+  res.status(200).json({"status":"running"});
+})
 app.get('/api/available-tickets', async (req, res) => {
   try {
     const totalTicketsSold = await Payment.aggregate([
